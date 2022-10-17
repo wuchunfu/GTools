387f95b2733fbd839a0e422c545cc4738af1e519
Pixiao <xiao15103183668@163.com>
2022-10-17 16:09:35 +0800

    修改部分说明

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,13 +1,11 @@
 <template>
-    <div>
-        123132
-        <n-button type="success">1234</n-button>
-        <n-button type="warning">324234</n-button>
-        <n-button >123123</n-button>
-        <n-divider />
-    </div>
+
 </template>
 
-<script setup>
+<script>
 
 </script>
+
+<style>
+
+</style>
